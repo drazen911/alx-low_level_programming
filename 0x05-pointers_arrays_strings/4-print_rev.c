c5af52577f04116db6585751a0e24740179825f8
drazen911 <omotayoolekan046@gmail.com>
2022-12-20 08:58:28 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_rev - This function prints a reverse string
+*@s: This is the input sring
+*/
+void print_rev(char *s)
+{
+	int index;
+	for (index = 0; s[index] != '\0'; index++);
+	for (index = index - 1; s[imdex] != '\0'; index--)
+	{
+		_putchar(s[index]);
+	}
+	_putchar('\n');
+}
